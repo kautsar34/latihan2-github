5c50162284d47e7454759ceed69dbd945d116a06
kautsar34 <kautsarputra04@gmail.com>
2022-02-09 14:20:47 +0700

    menambahkan paragraph

--- index.php
+++ index.php
@@ -8,5 +8,6 @@
 </head>
 <body>
     <h1>Hello World!</h1>
+    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Fuga quaerat optio eaque reprehenderit, perferendis nobis fugit quas dolor maiores vero tempore ut consectetur magnam impedit porro velit eum id neque?</p>
 </body>
 </html>
